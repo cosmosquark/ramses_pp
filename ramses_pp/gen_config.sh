2b6f7b0a3dd07d6fc8768fbd999aa344ee9fd91c
ben <ben@bloodstorm.(none)>
2014-07-02 20:38:35 +0100

    updates

--- ramses_pp/gen_config.sh
+++ ramses_pp/gen_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SCRIPT="`readlink -e $0`"
 SCRIPTPATH="`dirname $SCRIPT`"
+DIRPATH="$(dirname $SCRIPTPATH)"
 cp config.py.in config.py
-sed -i 's,'"..ramses_f90"','"${SCRIPTPATH}/ramses_f90"',g' config.py
-sed -i 's,'"..data"','"${SCRIPTPATH}/data"',g' config.py
+sed -i 's,'"..ramses_f90"','"${DIRPATH}/ramses_f90"',g' config.py
+sed -i 's,'"..data"','"${DIRPATH}/data"',g' config.py
