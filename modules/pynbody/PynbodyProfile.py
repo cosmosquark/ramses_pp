a66e917824bc88c72967e34656cd2f123aac72ca
David Sullivan <dsullivan0190@gmail.com>
2014-07-16 15:18:23 +0100

    Tipsy mass unit fix, removed my retarded import crap from Simulation.py, added some cosmo/constants stuff. Removed .pyc files

--- modules/pynbody/PynbodyProfile.py
+++ modules/pynbody/PynbodyProfile.py
@@ -5,7 +5,7 @@ Based on Pymses.py from the Hamu project https://github.com/samgeen/Hamu
 '''
 
 import pynbody
-from ...pynbody import Pynbody as PynbodySnapshot
+import Pynbody as PynbodySnapshot
 from .. import Profile
 import numpy as np
 import sys, os
@@ -50,32 +50,37 @@ class PynbodyProfile(Profile.Profile):
 
 	#TODO Filter by property e.g temperature
 	def fgas_halomass(self, halos=None, baryon_fraction=False):
-		s = self._snapshot.raw_snapshot()
-
-		s.physical_units()
+		cosmo = self._snapshot.cosmology()
+		h = cosmo['h']
 
 		if (halos == None):
-			halos = s.halos()
+			halos = self._snapshot.halos()
 
-		fgas = np.zeros(len(halos)) # len(halos) - 1 ?
-		mhalo = np.zeros(len(halos))
+		fgas = [] # len(halos) - 1 ?
+		mhalo = []
+
+		print 'Found %d halos'%len(halos)
 
 		#Loop over each halo and determine the gas fraction
-		i = 0
-		for halo in halos:
-			print 'Processing halo %d'%i
-			gas_mass = np.sum(halo.g['mass'])
-			stellar_mass = np.sum(halo.s['mass'])
-			total_mass = np.sum(halo['mass'])
-			if(verbose): print 'mg = %e, mtot = %e, fg = %d'%(gas_mass, total_mass, gas_mass/total_mass)
+		j = 0
+		try:
+			for halo in halos:
+				halo.physical_units()
+				print 'Processing halo %d'%j
+				gas_mass = np.sum(halo.g['mass']*h)
+				stellar_mass = np.sum(halo.s['mass']*h)
+				total_mass = np.sum(halo['mass']*h)
+				if(verbose): print 'mg = %e, mtot = %e, fg = %d'%(gas_mass, total_mass, gas_mass/total_mass)
 
-			#If baryon_fraction is true, calculate fraction of baryons in halo instead of just gas
-			mhalo[i] = total_mass
-			if baryon_fraction:
-				fgas[i] = (gas_mass + stellar_mass) / total_mass
-			else:
-				fgas[i] = gas_mass / total_mass
-			i = i + 1
-			#if i == 50: break
+				#If baryon_fraction is true, calculate fraction of baryons in halo instead of just gas
+				mhalo.append(total_mass)
+				if baryon_fraction:
+					fgas.append((gas_mass + stellar_mass) / total_mass)
+				else:
+					fgas.append(gas_mass / total_mass)
+				j = j + 1
+				#if j == 50: break
+		except KeyError:
+			print 'KeyError on halo %d'%j
 
-		return fgas, mhalo
+		return np.array(fgas), np.array(mhalo)
--- modules/utils/constants.py
+++ modules/utils/constants.py
@@ -1,2 +1,6 @@
 cmtokpc = 3.2407793e-22
 G_u = 4.4998712e-6 # G in kpc^3 / Msol / Gyr^2
+G = 6.67384e-11 # SI
+mp = 1.67262178e-27 # kg
+kb = 1.3806488e-23 # SI
+Msun = 1.9891e30 # kg
