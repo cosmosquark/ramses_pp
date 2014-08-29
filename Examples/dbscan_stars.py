373d493120bf5dd4322c26714e174980461912b4
David Sullivan <dsullivan0190@gmail.com>
2014-08-29 18:59:21 +0100

    Normalises positions of stars

--- Examples/dbscan_stars.py
+++ Examples/dbscan_stars.py
@@ -1,15 +1,27 @@
 from ramses_pp.modules import Simulation
 import numpy as np
+import sys
 
+ioutput = int(sys.argv[1])
 sim = Simulation.load('4Mpc_aton_256')
-snap = sim.snapshot(214, module='yt')
+snap = sim.snapshot(ioutput, module='yt')
 halos = snap.halos()
-h1 = halos[1]
+h_num = int(sys.argv[2])
+h1 = halos[h_num]
 
 db, X = h1.dbscan()
+print X
 labels = db.labels_
+
+#import sys
+#for label in labels:
+#	print label
+
+#sys.exit(1)
+
 core_samples = db.core_sample_indices_
 
+groups = []
 #Find stars that are in groups (omit noise)
 for k in set(labels):
 	if k == -1: continue # noise
@@ -18,12 +30,11 @@ for k in set(labels):
 	core_samples = [index for index in core_samples if labels[index] == k]
 
 	#Grab the position of each star in the group k and check if they are a core sample\
-	group = []
 	for index in class_members:
 		pos = X[index]
 		is_core_sample = (index in core_samples)
 
 		#Do some science...
-		group.append(pos)
+		groups.append([pos[0], pos[1], pos[2], k])
 
-	print group
+np.savetxt('./grps_%05d_%d.dat'%(ioutput, h_num), groups)
